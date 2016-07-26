c844b34a471bb953ca5f113599e9e0c8a1085e58
jatut <jatut_1@mail.ru>
2016-07-26 22:17:18 +0300

    2016-07-26_22:17

--- js/index.js
+++ js/index.js
@@ -46,3 +46,20 @@ function hidetext_slide(){
 $(document).ready(function() {
     showtext_slide();
 })
+
+/************************************ footer ************************************************/
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 2000);
+}
+
